1f457c83d98173b53ed1975fdadcaa4fdff76099
dopewevmond <dopewevmond@gmail.com>
2022-05-06 12:28:18 +0000

    updated a deprecated method

--- app/models.py
+++ app/models.py
@@ -62,7 +62,7 @@ class User(db.Model):
     visits = db.relationship('Location', secondary=visited, backref='wasvisitedby')
 
     def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
+        self.password_hash = pwd_context.hash(password)
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
