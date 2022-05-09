964025464d370ba3686a069e7992545e8ffb1fd3
dopewevmond <dopewevmond@gmail.com>
2022-05-09 21:28:27 +0000

    mounted admin blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -17,9 +17,11 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     from app.auth import bp as auth_bp
     from app.errors import bp as errors_bp
+    from app.admin import bp as admin_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(errors_bp)
+    app.register_blueprint(admin_bp)
 
     return app
--- app/errors/error_handler.py
+++ app/errors/error_handler.py
@@ -7,6 +7,5 @@ def internal_error(error):
     return {"message": str(error), "data": None, "error": "Internal server error"}, 500
 
 @bp.app_errorhandler(404)
-def internal_error(error):
-    db.session.rollback()
+def not_found_error(error):
     return {"message": str(error), "data": None, "error": "Not found"}, 404
