2b11a15f6621668b8a851be9a74a5e83f6226cf9
dopewevmond <dopewevmond@gmail.com>
2022-06-15 01:13:53 +0000

    allow admins to verify user accounts

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -4,6 +4,7 @@ from app.auth.routes import admin_access_required
 from flask_restful import Api, Resource, abort, reqparse, fields, marshal
 from app.models import User, TestingCenter
 
+
 api = Api(bp)
 
 testing_center_fields = {
@@ -11,6 +12,20 @@ testing_center_fields = {
     'name': fields.String,
     'region': fields.String,
     'constituency': fields.String
+}
+user_fields =  {
+    'id': fields.Integer,
+    'first_name': fields.String,
+    'last_name': fields.String,
+    'username': fields.String,
+    'email': fields.String,
+    'gender': fields.Boolean,
+    'dob': fields.DateTime,
+    'phone_number': fields.String,
+    'is_verified': fields.Boolean,
+    'is_admin': fields.Boolean,
+    'id_type': fields.String,
+    'id_number': fields.String
 }
 
 class TestingCenterListAPI(Resource):
@@ -85,22 +100,43 @@ class TestingCenterAPI(Resource):
         db.session.delete(center)
         db.session.commit()
         return {"message": "Deleted successfully", "error": None, "data": None}
-        
 
 
-class SearchTestingCenterAPI(Resource):
-    def get(self):
+class VerifyUserAPI(Resource):
+    decorators = [admin_access_required]
+
+    def get(self, current_user, id):
+        user = User.query.get(id)
+        user_data = marshal(user, user_fields)
+        user_data['link'] = user.image_link_for_verification
+        return {"message": "User data found", "data": user_data}
+
+    def post(self, current_user, id):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('region', type=str, required=True, help='Add region to search for testing center', location='json')
+        self.reqparse.add_argument('correct_user_creds', type=bool, required=True, help='User verification eligibilty status missing', location='json')
         args = self.reqparse.parse_args()
-        
-        region = args['region']
-        if not region:
+        correct_user_creds = args['correct_user_creds']
+
+        if not correct_user_creds:
             abort(400)
         
-        return marshal(TestingCenter.search_by_region(region).all(), testing_center_fields)
-
+        try:
+            user = User.query.get(id)
+            if not user:
+                abort(404)
+            if correct_user_creds:
+                user.is_verified = True
+            user.revoke_verification_request()
+            db.session.commit()
+            return {"message": "User verified successfully", "error": None, "data": {"id": user.id}}, 200
+        except Exception as e:
+            print(e)
+            return {
+                "message": "Something went wrong",
+                "error": str(e),
+                "data": None
+            }, 400
 
 api.add_resource(TestingCenterListAPI, '/testing-centers', endpoint='testing_center_list')
 api.add_resource(TestingCenterAPI, '/testing-centers/<int:id>', endpoint='testing_centers')
-api.add_resource(SearchTestingCenterAPI, '/search-testing-center', endpoint='search_testing_center')
+api.add_resource(VerifyUserAPI, '/verify-user/<int:id>', endpoint='verify_user')
